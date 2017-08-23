2e38b19ebe8837bd8f00957ba8d68d0bcc90f36c
sunilbhatraju <sunil.bhatraju@gmail.com>
2017-08-23 17:23:27 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  
+selector: 'app-root',
+  
+templateUrl: './app.component.html',
+  
+styleUrls: ['./app.component.css']
+
+})
+
+export class AppComponent {
+  
+title = 'Employee Details';
+
+employees = [
+{name :"Sunil",  position : "Developer", salary : 20000},
+{name :"Vamshi", position : "Java Developer",  salary : 30000},
+{name : "Chethan", position : ".Net Developer",
+salary : 10000}
+];
+model:any={};
+addEmployee()
+{
+	this.employees.push(this.model);
+	this.model = {};
+}
+
+deleteEmployee(i){
+this.employees.splice(i,1)
+console.log(i);
+}
+}
