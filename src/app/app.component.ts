3161640c6962470eb724d33dd3fb0f3d59d83cbc
sunilbhatraju <sunil.bhatraju@gmail.com>
2017-08-30 19:03:59 +0530

    Update app.component.ts
    updated by adding edit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   
 selector: 'app-root',
@@ -8,19 +11,21 @@ templateUrl: './app.component.html',
   
 styleUrls: ['./app.component.css']
 
+
 })
 
 export class AppComponent {
   
+
 title = 'Employee Details';
 
-employees = [
-{name :"Sunil",  position : "Developer", salary : 20000},
+employees = [{name :"Sunil",  position : "Developer", salary : 20000},
 {name :"Vamshi", position : "Java Developer",  salary : 30000},
 {name : "Chethan", position : ".Net Developer",
-salary : 10000}
-];
+salary : 10000}];
+
 model:any={};
+model2:any={};
 addEmployee()
 {
 	this.employees.push(this.model);
@@ -31,4 +36,22 @@ deleteEmployee(i){
 this.employees.splice(i,1)
 console.log(i);
 }
+myValue;
+editEmployee(k){
+this.model2.name = this.employees[k].name;
+this.model2.position = this.employees[k].position;
+this.model2.salary = this.employees[k].salary;
+this.myValue = k;
+}
+updateEmployee(){
+let k = this.myValue;
+for(let i=0;i<this.employees.length;i++){
+	if(i==k)
+{
+	this.employees[i]= this.model2;
+	this.model2 = {};
+}
+}
+}
+
 }
