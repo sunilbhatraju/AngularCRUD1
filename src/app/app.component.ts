360438cea435f1b585a46549f872666f402de636
sunilbhatraju <sunil.bhatraju@gmail.com>
2017-09-04 11:44:00 +0530

    Update app.component.ts
    Validators added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,84 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup , Validators} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @Component({
-  
-selector: 'app-root',
-  
-templateUrl: './app.component.html',
-  
-styleUrls: ['./app.component.css']
 
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css']
 
 })
 
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  model:any={};
+  model2:any={};
+  edit= false;
+  add=false;
+  create=true;
+  Show=false;
+  myValue;
+  ngOnInit(){
+    this.model = new FormGroup({
+      'name': new FormControl(this.model.name, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      'position': new FormControl(this.model.position),
+      'salary': new FormControl(this.model.salary, Validators.required)
+    });
+  }
+  title = 'Welcome to the Site';
+  employees = [{name :"Sunil",  position : "Developer", salary : 20000},
+    {name :"Vamshi", position : "Java Developer",  salary : 30000},
+    {name : "Chethan", position : ".Net Developer", salary : 10000}];
+  createEmp(){
+   this.add=true;
+   this.create=false;
+    this.Show=false;
+    this.edit=false;
+  }
+  addEmployee()
+  {
+    console.log()
+    this.employees.push(this.model);
+    this.Show = true;
+ this.add=false;
+    this.model = {};
+  }
 
-title = 'Employee Details';
-
-employees = [{name :"Sunil",  position : "Developer", salary : 20000},
-{name :"Vamshi", position : "Java Developer",  salary : 30000},
-{name : "Chethan", position : ".Net Developer",
-salary : 10000}];
-
-model:any={};
-model2:any={};
-addEmployee()
-{
-	this.employees.push(this.model);
-	this.model = {};
+  deleteEmployee(i){
+    this.employees.splice(i,1)
+    this.Show=true;
+    console.log(i);
+  }
+  editEmployee(k){
+    this.edit = true;
+    this.Show=false;
+    this.add=false;
+    if(name!="")
+    {
+      this.model2.name = this.employees[k].name;
+    }
+    this.model2.position = this.employees[k].position;
+    this.model2.salary = this.employees[k].salary;
+    this.myValue = k;
+  }
+  updateEmployee(){
+    this.Show=true;
+    this.edit = false;
+    let k = this.myValue;
+    for(let i=0;i<this.employees.length;i++){
+      if(i==k)
+      {
+        this.employees[i]= this.model2;
+        this.model2 = {};
+      }
+    }
+  }
 }
 
-deleteEmployee(i){
-this.employees.splice(i,1)
-console.log(i);
-}
-myValue;
-editEmployee(k){
-this.model2.name = this.employees[k].name;
-this.model2.position = this.employees[k].position;
-this.model2.salary = this.employees[k].salary;
-this.myValue = k;
-}
-updateEmployee(){
-let k = this.myValue;
-for(let i=0;i<this.employees.length;i++){
-	if(i==k)
-{
-	this.employees[i]= this.model2;
-	this.model2 = {};
-}
-}
-}
-
-}
